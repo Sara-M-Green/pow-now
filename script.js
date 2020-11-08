c300c75abcf31983cd4889327ea4b9d6667d9ba9
Sara Green <smg105c@gmail.com>
2020-11-08 15:36:40 -0800

    cleaned up code and added comments

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//api key for world weather online
-const apiKey = 'cadbd16055d546e9afd42652200211';
-
 //list of resorts to search snowfall data
 const resortList = [
     {
@@ -61,44 +58,44 @@ function getWeatherData(resortList){
 
 //makes a request to world wide weather API
 function findWeather(city){
+    const apiKey = 'cadbd16055d546e9afd42652200211'
     const url =  `https://api.worldweatheronline.com/premium/v1/ski.ashx?key=${apiKey}&q=${city}&includeLocation=yes&format=json`
     return fetch(url)
     .then(response => {
         if (response.ok){
-            return response.json();
+            return response.json()
         }
-        throw new Error(response.statusText);
+        throw new Error(response.statusText)
     })
 }
 
 //displays weather results to HTML
 function displayResults(weatherDataArray){
-    console.log(weatherDataArray)
-    $('#results-list').empty();
+    $('#results-list').empty()
     for(let i = 0; i < weatherDataArray.length; i++){
-    let snowIn = (`${weatherDataArray[i].data.weather[0].totalSnowfall_cm}`*0.39370079).toFixed(2);    
+    let snowIn = (`${weatherDataArray[i].data.weather[0].totalSnowfall_cm}`*0.39370079).toFixed(2)  
         $('#results-list').append(
-            `<div class="resort-result">
-                <h3 class="resortName">${resortList[i].resort}</h3>
+            `<div class='resort-result'>
+                <h3 class='resortName'>${resortList[i].resort}</h3>
                 <h4>${resortList[i].cityState}</h4>
                 
-                    <p class="snowfall">Current Snowfall: ${snowIn} in</p>
+                    <p class='snowfall'>Current Snowfall: ${snowIn} in</p>
                 
-                <div id="flightPrice-${i}">
+                <div id='flightPrice-${i}'>
                 </div>
             </div>`
-        );     
-    };
-    $('#results').removeClass('hidden');
-    $('#home-screen-copy').addClass('hidden');
-    $('#submit').attr("value", "Search Again")
+        )    
+    }
+    $('#results').removeClass('hidden')
+    $('#home-screen-copy').addClass('hidden')
+    $('#submit').attr('value', 'Search Again')
 }
 
 //loops through resort list and finds inbound airport code to each resort
 function getFlightData(resortList){
     for(i = 0; i < resortList.length; i++){
-        let resortName = resortList[i].resort;
-        let inbound = resortList[i].airport; 
+        let resortName = resortList[i].resort
+        let inbound = resortList[i].airport
         findFlights(inbound, resortName)
     }
 }
@@ -106,73 +103,73 @@ function getFlightData(resortList){
 //makes request for flight info from selected outbound airport to resort
 function findFlights(inbound, resortName){
 
-    let outbound = $('#outbound-airport').val();
-    outboundDate = createTomorrowsDate();
-    inboundDate = $('#inbound-date').val();
+    const outbound = $('#outbound-airport').val()
+    const outboundDate = createTomorrowsDate()
+    const inboundDate = $('#inbound-date').val()
 
-    let url = `https://rapidapi.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${outbound}-sky/${inbound}-sky/${outboundDate}?inboundpartialdate=${inboundDate}`;
+    const url = `https://rapidapi.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${outbound}-sky/${inbound}-sky/${outboundDate}?inboundpartialdate=${inboundDate}`
     
     fetch(url, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "36f9ae898amsh1413f301961a480p18ed20jsn9e5d4d694fec",
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        'method': 'GET',
+        'headers': {
+            'x-rapidapi-key': '36f9ae898amsh1413f301961a480p18ed20jsn9e5d4d694fec',
+            'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com'
         }
     })
     .then(response => {
-        return response.json();
+        return response.json()
     })
     .then(responseJsonFlight => {
-        console.log(responseJsonFlight)
-        findMinPrice(responseJsonFlight, resortName);
+        findMinPrice(responseJsonFlight, resortName)
     })
     .catch(err => {
-        console.error(err);
-    });  
+        console.error(err)
+    })
 }
 
-
+//finds price and carrier from flight data response and appends results to HTML
 function findMinPrice(responseJsonFlight, resortName){
     const matchingIndex = resortList.findIndex(resort => resort.resort === resortName)
-   if(responseJsonFlight.Quotes.length === 0){
+    if(responseJsonFlight.Quotes.length === 0){
         $(`#flightPrice-${matchingIndex}`).append(`<p>No flights found into ${resortList[matchingIndex].airportName}</p>`)
     } else {
-        let price = responseJsonFlight.Quotes[0].MinPrice
-        $(`#flightPrice-${matchingIndex}`).append(`<p>Flight Price: $${price}</p> <p>Into ${resortList[matchingIndex].airportName}</p>`);
+        const price = responseJsonFlight.Quotes[0].MinPrice
+        const airline = responseJsonFlight.Carriers[0].Name
+        $(`#flightPrice-${matchingIndex}`).append(`<p class='flight-price'>Flight Price: $${price}</p> 
+        <p>Into ${resortList[matchingIndex].airportName}</p>
+        <p>on ${airline}</p>`)
     } 
 }
 
-
-
 // creates tomorrow's date variable for outbound flight date
 function createTomorrowsDate(){
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate() + 1;
+    const date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate() + 1
     if(month < 10){
-        month = '0' + month;
+        month = '0' + month
     }
     if (day < 10){
-        day = '0' + day;
+        day = '0' + day
     }
-    let tomorrow = `${year}-${month}-${day}`;
-    return tomorrow;
+    const tomorrow = `${year}-${month}-${day}`
+    return tomorrow
 }
 
 // sets the date input's min value
 function minDateAttr(){
-    let minDate = createTomorrowsDate()
-    $('#inbound-date').attr("min", minDate)
+    const minDate = createTomorrowsDate()
+    $('#inbound-date').attr('min', minDate)
 }
 
 //  submit click listener function
 function watchForm(){
     $('form').submit(event => {
-        event.preventDefault();
-        getWeatherData(resortList);
-    });
+        event.preventDefault()
+        getWeatherData(resortList)
+    })
 }
 
-$(watchForm);
-$(minDateAttr);
+$(watchForm)
+$(minDateAttr)
