430370e19ef3f4e8a8909fdeabb4e464027be3a0
Sara Green <smg105c@gmail.com>
2020-11-08 13:33:22 -0800

    added airport name to results

--- script.js
+++ script.js
@@ -7,39 +7,45 @@ const resortList = [
         resort: 'Mammoth Mountain',
         cityState: 'Mammoth Lakes, CA',
         searchQuery: 'mammoth+lakes',
-        airport: 'MMH'
+        airport: 'MMH',
+        airportName: 'Mammoth Yosemite Airport'
     },
     {
         resort: 'Telluride',
         cityState: 'Telluride, CO',
         searchQuery: 'telluride',
-        airport: 'MTJ'
+        airport: 'MTJ',
+        airportName: 'Montrose Regional Airport'
     },
     {
         resort: 'Squaw Valley/Alpine Meadows',
         cityState: 'Olympic Valley, CA',
         searchQuery: 'olympic+valley',
-        airport: 'RNO'
+        airport: 'RNO',
+        airportName: 'Reno-Tahoe International Airport'
     },
     {
-        resort: 'Big Sky',
+        resort: 'Big Sky Resort',
         cityState: 'Big Sky, MT',
         searchQuery: 'big+sky',
-        airport: 'BZN'
+        airport: 'BZN',
+        airportName: 'Bozeman Yellowstone International Airport'
     },
     {
         resort: 'Snowbird',
         cityState: 'Salt Lake City, UT',
         searchQuery: 'salt+lake+city',
-        airport: 'SLC'
+        airport: 'SLC',
+        airportName: 'Salt Lake City International Airport'
     },
     {
         resort: 'Sun Valley',
         cityState: 'Sun Valley, ID',
         searchQuery: 'sun+valley',
-        airport: 'SUN'
+        airport: 'SUN',
+        airportName: 'Friedman Memorial Airport'
     }
-];
+]
 
 //loops through resort list and finds weather data for each resort
 function getWeatherData(resortList){
@@ -57,12 +63,12 @@ function getWeatherData(resortList){
 function findWeather(city){
     const url =  `https://api.worldweatheronline.com/premium/v1/ski.ashx?key=${apiKey}&q=${city}&includeLocation=yes&format=json`
     return fetch(url)
-        .then(response => {
-            if (response.ok){
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
 }
 
 //displays weather results to HTML
@@ -73,10 +79,10 @@ function displayResults(weatherDataArray){
     let snowIn = (`${weatherDataArray[i].data.weather[0].totalSnowfall_cm}`*0.39370079).toFixed(2);    
         $('#results-list').append(
             `<div class="resort-result">
-                <h3>${resortList[i].resort}</h3>
+                <h3 class="resortName">${resortList[i].resort}</h3>
                 <h4>${resortList[i].cityState}</h4>
                 
-                    <p>Current Snowfall: <strong>${snowIn} in</strong></p>
+                    <p class="snowfall">Current Snowfall: ${snowIn} in</p>
                 
                 <div id="flightPrice-${i}">
                 </div>
@@ -129,10 +135,10 @@ function findFlights(inbound, resortName){
 function findMinPrice(responseJsonFlight, resortName){
     const matchingIndex = resortList.findIndex(resort => resort.resort === resortName)
    if(responseJsonFlight.Quotes.length === 0){
-        $(`#flightPrice-${matchingIndex}`).append(`<p>No flights found into ${resortList[matchingIndex].airport}</p>`)
+        $(`#flightPrice-${matchingIndex}`).append(`<p>No flights found into ${resortList[matchingIndex].airportName}</p>`)
     } else {
-            let price = responseJsonFlight.Quotes[0].MinPrice
-            $(`#flightPrice-${matchingIndex}`).append(`<p>Flight Price: $${price} into ${resortList[matchingIndex].airport}</p>`);
+        let price = responseJsonFlight.Quotes[0].MinPrice
+        $(`#flightPrice-${matchingIndex}`).append(`<p>Flight Price: $${price}</p> <p>Into ${resortList[matchingIndex].airportName}</p>`);
     } 
 }
 
